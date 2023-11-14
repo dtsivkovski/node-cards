19299473f7cf66eceb4270ceca33f17c0132cd8d
Daniel Tsivkovski <dtsivkovs@gmail.com>
2023-11-14 02:28:57 -0800

    mongodb and server

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const { mongoose } = require("mongoose");
+const mongoURI = process.env.mongoURI;
+console.log(mongoURI);
 var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+mongoose.connect(mongoURI);
+mongoose.connection.on("connected", () => {
+    console.log("Mongoose DB Connected");
+});
 
 // use res.render to load up an ejs view file
 
